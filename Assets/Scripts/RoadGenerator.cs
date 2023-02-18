35787ebe01031e7aa29830858ca7b6f8f8b98420
Defnel1s <mrsfrenk@gmail.com>
2023-02-18 13:34:35 +0900

    Runner

---
+++ Assets/Scripts/RoadGenerator.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    public GameObject RoadPrefab;
+    private List<GameObject> roads = new List<GameObject>();
+    public float maxSpeed = 10;
+    private float speed =0;
+    public int maxRoadCount = 5;
+
+    private void Start()
+    {
+        ResetLevel();
+        StartLevel();
+    }
+    private void Update()
+    {
+        if (speed == 0) return;
+
+        foreach (GameObject road in roads)
+        {
+            road.transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+        }
+        if (roads[0].transform.position.z < -19.75771f)
+        {
+            Destroy(roads[0]);
+            roads.RemoveAt(0);
+
+            CreateNextRoad();
+        }
+    }
+    private void CreateNextRoad()
+    {
+        Vector3 pos = Vector3.zero;
+        if (roads.Count > 0) { pos = roads[roads.Count - 1].transform.position + new Vector3(0, 0, 19.75771f); }
+        GameObject go = Instantiate(RoadPrefab, pos, Quaternion.identity);
+        go.transform.SetParent(transform);
+        roads.Add(go);
+    }
+    public void StartLevel()
+    {
+        speed = maxSpeed;
+    }
+    public void ResetLevel()
+    {
+        speed = 0;
+        while (roads.Count > 0)
+        {
+            Destroy(roads[0]);
+            roads.RemoveAt(0);
+        }
+        for (int i = 0; i< maxRoadCount; i++)
+        {
+            CreateNextRoad();
+        }
+    }
+}
